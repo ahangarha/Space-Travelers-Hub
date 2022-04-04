662940cd50c750055368d75133708f5e3453777a
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-04 17:15:10 +0300

    add thunk and middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import MissionsReducer from './missions/missions';
 
 const rootReducer = combineReducers({
   missions: MissionsReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware([
+    logger,
+    thunk,
+  ]),
+);
 
 export default store;
