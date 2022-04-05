bf46bd299e9a6ada41fc720a832e91201491c9a0
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-05 18:32:03 +0300

    add unit test for joininf mission

--- src/redux/__tests__/Missions.test.js
+++ src/redux/__tests__/Missions.test.js
@@ -1,4 +1,4 @@
-import reducer, { fetchMissionsSuccess } from '../missions/missions';
+import reducer, { fetchMissionsSuccess, joinMission } from '../missions/missions';
 
 describe('Test reducer and actions', () => {
   it('Reutrns initial state with no action', () => {
@@ -12,5 +12,40 @@ describe('Test reducer and actions', () => {
     }];
 
     expect(reducer([], fetchMissionsSuccess(missions))).toEqual(missions);
+  });
+});
+
+describe('Join a mission', () => {
+  it('Can join mission a mission', () => {
+    const initialState = [
+      {
+        id: '1',
+        joined: false,
+      },
+      {
+        id: '2',
+        joined: false,
+      },
+      {
+        id: '3',
+        joined: false,
+      },
+    ];
+    const expectedState = [
+      {
+        id: '1',
+        joined: false,
+      },
+      {
+        id: '2',
+        joined: true,
+      },
+      {
+        id: '3',
+        joined: false,
+      },
+    ];
+
+    expect(reducer(initialState, joinMission('2'))).toEqual(expectedState);
   });
 });
