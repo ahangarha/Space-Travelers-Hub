090a78f933b9c8767f6c219b13d84360e9c937b2
BonkeG <bonkegcobo291195@gmail.com>
2022-04-05 13:48:53 +0200

    fetch data from server

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,6 +1,21 @@
-const nameInitialState = [];
+import axios from 'axios';
 
-const rocketReducer = (state = nameInitialState, action) => {
+const InitialState = [];
+const FETCH_ROCKETS = 'FetchRockets';
+
+const baseUrl = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = () => async (dispatch) => {
+  const rockets = await axios.get(baseUrl);
+  const mappedRockets = rockets.data.map((rocket) => ({
+    id: rocket.rocket_id,
+    name: rocket.rocket_name,
+    type: rocket.rocket_type,
+    images: rocket.flickr_images[0],
+  }));
+};
+
+const rocketReducer = (state = InitialState, action) => {
   switch (action.type) {
     default:
       return state;
