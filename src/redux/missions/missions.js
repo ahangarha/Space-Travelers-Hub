2f73250e7e74505fdc8e05d4a384ee1fddad5c6a
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-05 14:22:32 +0300

    add relevant actions for fetching missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,5 +1,31 @@
+const FETCH_MISSIONS_SUCCESS = 'spaceTraveler/missions/FETCH_MISSIONS_SUCCESS';
+
+const fetchMissionsSuccess = (missions) => ({
+  type: FETCH_MISSIONS_SUCCESS,
+  payload: {
+    missions,
+  },
+});
+
+export const fetchMissions = () => (dispatch) => {
+  const API_URL = 'https://api.spacexdata.com/v3/missions';
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((res) => {
+      const missions = res.map((r) => ({
+        id: r.mission_id,
+        name: r.mission_name,
+        description: r.description,
+        joined: false,
+      }));
+      dispatch(fetchMissionsSuccess(missions));
+    });
+};
+
 export default function reducer(state = [], action) {
   switch (action.type) {
+    case FETCH_MISSIONS_SUCCESS:
+      return [...state, ...action.payload.missions];
     default:
       return state;
   }
