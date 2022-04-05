72dfde89b926df37d1bcbe550c1338e6ba8f11e8
BonkeG <bonkegcobo291195@gmail.com>
2022-04-05 13:56:09 +0200

    Create action for rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 const InitialState = [];
-const FETCH_ROCKETS = 'FetchRockets';
+const FETCH_ROCKETS = 'FETCH_ROCKETS';
 
 const baseUrl = 'https://api.spacexdata.com/v3/rockets';
 
-export const fetchRockets = () => async (dispatch) => {
+const fetchRockets = (mappedRockets) => ({
+  type: FETCH_ROCKETS,
+  payload: {
+    mappedRockets,
+  },
+});
+
+export const fetchRocketsFromServer = () => async (dispatch) => {
   const rockets = await axios.get(baseUrl);
   const mappedRockets = rockets.data.map((rocket) => ({
     id: rocket.rocket_id,
@@ -13,6 +20,7 @@ export const fetchRockets = () => async (dispatch) => {
     type: rocket.rocket_type,
     images: rocket.flickr_images[0],
   }));
+  dispatch(fetchRockets(mappedRockets));
 };
 
 const rocketReducer = (state = InitialState, action) => {
