bc608b540aeba058e64d65f48e4483805314f685
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-06 15:11:43 +0300

    add unit test for leave mission

--- src/redux/__tests__/Missions.test.js
+++ src/redux/__tests__/Missions.test.js
@@ -1,4 +1,4 @@
-import reducer, { fetchMissionsSuccess, joinMission } from '../missions/missions';
+import reducer, { fetchMissionsSuccess, joinMission, leaveMission } from '../missions/missions';
 
 describe('Test reducer and actions', () => {
   it('Reutrns initial state with no action', () => {
@@ -15,8 +15,8 @@ describe('Test reducer and actions', () => {
   });
 });
 
-describe('Join a mission', () => {
-  it('Can join mission a mission', () => {
+describe('Join and leave a mission', () => {
+  it('Can join mission', () => {
     const initialState = [
       {
         id: '1',
@@ -47,5 +47,38 @@ describe('Join a mission', () => {
     ];
 
     expect(reducer(initialState, joinMission('2'))).toEqual(expectedState);
+  });
+
+  it('Can leave mission', () => {
+    const initialState = [
+      {
+        id: '1',
+        joined: true,
+      },
+      {
+        id: '2',
+        joined: true,
+      },
+      {
+        id: '3',
+        joined: false,
+      },
+    ];
+    const expectedState = [
+      {
+        id: '1',
+        joined: true,
+      },
+      {
+        id: '2',
+        joined: false,
+      },
+      {
+        id: '3',
+        joined: false,
+      },
+    ];
+
+    expect(reducer(initialState, leaveMission('2'))).toEqual(expectedState);
   });
 });
