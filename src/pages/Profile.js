5ee469301a61cc91a8906cc2a7a3dcbd5a97ee2f
BonkeG <bonkegcobo291195@gmail.com>
2022-04-06 20:54:52 +0200

    Add reserved booking to the profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,7 @@ import './Profile.css';
 
 function Profile() {
   const missions = useSelector((state) => (state.missions.filter((mission) => mission.joined)));
+  const rockets = useSelector((state) => (state.rockets.filter((rocket) => rocket.reserved)));
 
   return (
     <section className="profile-page-container container">
@@ -13,6 +14,19 @@ function Profile() {
             missions.map((mission) => (
               <li className="item" key={mission.id}>
                 {mission.name}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div>
+        <h2>Rockets</h2>
+        <ul className="items-list">
+          {
+            rockets.map((rocket) => (
+              <li className="item" key={rocket.id}>
+                {rocket.name}
               </li>
             ))
           }
