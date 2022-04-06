0e2b7a50375b691998f6140a3de11e357be370f8
Mostafa Ahangarha <ahangarha@riseup.net>
2022-04-06 15:39:34 +0300

    show missions on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
+import './Profile.css';
 
 function Profile() {
+  const missions = useSelector((state) => (state.missions));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!missions.length) dispatch(fetchMissions());
+  }, []);
+
   return (
-    <div>Profile</div>
+    <section className="profile-page-container container">
+      <div>
+        <h2>My Missions</h2>
+        <ul className="items-list">
+          {
+            missions.map((mission) => (
+              <li className="item" key={mission.id}>
+                {mission.name}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </section>
   );
 }
 
